b2fc8b44ca56d7007484daabbb74d08db99f4532
esadakman <esadd26@gmail.com>
2022-07-24 21:06:21 +0300

    table pagination

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -8,11 +8,15 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditIcon from "@mui/icons-material/Edit";
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, TablePagination } from "@mui/material";
 import { ref, remove } from "firebase/database";
 import { db } from "../../utils/firebase";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "100% !important",
+  },
   tableContainer: {
     borderRadius: 15,
     bgcolor: "red",
@@ -45,6 +49,20 @@ export default function CustomizedTables({
 }) {
   const classes = useStyles();
 
+  // * Pagination
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
   // ? DELETE
   const handleDelete = (data) => {
     remove(ref(db, `/${data.uuid}`));
@@ -57,7 +75,7 @@ export default function CustomizedTables({
   };
 
   return (
-    <Container>
+    <Container className={classes.container}>
       <Typography component="h1" variant="h4" className={classes.title}>
         Contact List
       </Typography>
@@ -73,27 +91,43 @@ export default function CustomizedTables({
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((info, index) => (
-              <TableRow key={index}>
-                <TableCell
-                  component="th"
-                  scope="row"
-                  sx={{ maxWidth: "130px", overflow: "auto" }}
-                >
-                  {info.name}
-                </TableCell>
-                <TableCell align="right">{info.tel}</TableCell>
-                <TableCell align="right">{info.gender}</TableCell>
-                <TableCell align="right" onClick={() => handleDelete(info)}>
-                  <DeleteForeverIcon className={classes.buttons} />
-                </TableCell>
-                <TableCell align="right" onClick={() => handleEdit(info)}>
-                  <EditIcon className={classes.buttons} />
-                </TableCell>
+            {data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((info, index) => (
+                <TableRow key={index}>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    sx={{ maxWidth: "130px", overflow: "auto" }}
+                  >
+                    {info.name}
+                  </TableCell>
+                  <TableCell align="right">{info.tel}</TableCell>
+                  <TableCell align="right">{info.gender}</TableCell>
+                  <TableCell align="right" onClick={() => handleDelete(info)}>
+                    <DeleteForeverIcon className={classes.buttons} />
+                  </TableCell>
+                  <TableCell align="right" onClick={() => handleEdit(info)}>
+                    <EditIcon className={classes.buttons} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 62 * emptyRows }}>
+                <TableCell colSpan={6} />
               </TableRow>
-            ))}
+            )}
           </TableBody>
         </Table>
+        <TablePagination
+          component="div"
+          count={data.length}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          rowsPerPageOptions={[5, 10, 15]}
+        />
       </TableContainer>
     </Container>
   );
